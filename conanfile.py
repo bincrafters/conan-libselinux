f1c95af5b87b7dc66eaf8b15407b7fc732e809d1
Croydon <git@cr0ydon.com>
2020-03-18 21:28:52 +0100

    Use conandata.yml

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,43 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
 from conans.errors import ConanInvalidConfiguration
 import os
+import glob
 
 
 class LibSELinuxConan(ConanFile):
     name = "libselinux"
-    version = "2.9"
     description = "Security-enhanced Linux is a patch of the Linux kernel and a number of utilities with enhanced security functionality designed to add mandatory access controls to Linux"
     topics = ("conan", "selinux", "security-enhanced linux")
     url = "https://github.com/bincrafters/conan-libselinux"
     homepage = "https://github.com/SELinuxProject/selinux"
     license = "Unlicense"  # This library (libselinux) is public domain software, i.e. not copyrighted
-    exports = ["LICENSE.md"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    _sepol_subfolder = "libsepol-%s" % version
-    _selinux_subfolder = "libselinux-%s" % version
-    _date = "20190315"
     requires = ("pcre2/10.33",)
+
+    def _get_subfolders(self):
+        _sepol_subfolder = "libsepol-%s" % self.version
+        _selinux_subfolder = "libselinux-%s" % self.version
+        return _sepol_subfolder, _selinux_subfolder
 
     def configure(self):
         del self.settings.compiler.libcxx
         del self.settings.compiler.cppstd
         if self.settings.os != "Linux":
-            raise ConanInvalidConfiguration("only Linux is supported")
+            raise ConanInvalidConfiguration("Only Linux is supported")
 
     def build_requirements(self):
         if not tools.which("flex"):
             self.build_requires("flex_installer/2.6.4@bincrafters/stable")
 
     def source(self):
-        source_url = "https://github.com/SELinuxProject/selinux/releases/download/%s/libselinux-%s.tar.gz" % (self._date, self.version)
-        tools.get(source_url, sha256="1bccc8873e449587d9a2b2cf253de9b89a8291b9fbc7c59393ca9e5f5f4d2693")
-        source_url = "https://github.com/SELinuxProject/selinux/releases/download/%s/libsepol-%s.tar.gz" % (self._date, self.version)
-        tools.get(source_url, sha256="a34b12b038d121e3e459b1cbaca3c9202e983137819c16baf63658390e3f1d5d")
+        for download in self.conan_data["sources"][self.version]:
+            tools.get(**download)
+            extracted_dir = glob.glob("lib*")[0]
 
     def build(self):
+        _sepol_subfolder, _selinux_subfolder = self._get_subfolders()
         pcre_inc = os.path.join(self.deps_cpp_info["pcre2"].rootpath,
                                 self.deps_cpp_info["pcre2"].includedirs[0])
         pcre_libs = ' '.join(["-l%s" % lib for lib in self.deps_cpp_info["pcre2"].libs])
@@ -53,6 +54,7 @@ class LibSELinuxConan(ConanFile):
             env_build.make(args=args)
 
     def package(self):
+        _sepol_subfolder, _selinux_subfolder = self._get_subfolders()
         self.copy(pattern="LICENSE", dst="licenses", src=self._selinux_subfolder)
         for library in [self._sepol_subfolder, self._selinux_subfolder]:
             self.copy(pattern="*.h", dst="include", src=os.path.join(library, "include"), keep_path=True)
